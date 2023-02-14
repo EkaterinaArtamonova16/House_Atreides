3c8c69c8920ce54e1ea428fa35aade9e7ed4519a
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-14 13:51:08 +0500

    added new tasks from 2 seminar

---
+++ 2 seminar C#/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter first number ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a%b==0)
+Console.WriteLine("True");
+
+else 
+
+Console.WriteLine(a%b);
+
+
+Console.WriteLine("Enter number ");
+int e = Convert.ToInt32(Console.ReadLine());
+int d = 7;
+int r = 23;
+
+if (e%d==0&&e%r==0)
+
+Console.WriteLine("True");
+
+else 
+
+Console.WriteLine("False");
