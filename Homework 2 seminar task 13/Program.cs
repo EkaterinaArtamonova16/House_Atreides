8742f18b7b8aa8db695a2909382f0bebde9f9203
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-14 15:32:51 +0500

    added task 13 to homework

---
+++ Homework 2 seminar task 13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+//сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ReadArray(char[]array)
+{
+    for (int i=0 ; i<array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+char[] arr = num.ToString().ToCharArray();
+ReadArray(arr);
+Console.WriteLine(arr[2]);
