524ab13b610a4ad94a64a2c0a7cee8eb5c0e4363
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-20 23:40:06 +0500

    finished 21

--- Homework 3 seminat task 21/Program.cs
+++ Homework 3 seminat task 21/Program.cs
@@ -3,4 +3,24 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Input x1");
+double x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input y1");
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input z1");
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input x2");
+double x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input y1");
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input z1");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1,2));
+
+Console.WriteLine("Answer" + result);
