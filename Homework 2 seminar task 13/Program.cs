09482280e0bc647e5105ea87fb0bdd5aa458dfcc
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-20 16:21:03 +0500

    added new task 15

--- Homework 2 seminar task 13/Program.cs
+++ Homework 2 seminar task 13/Program.cs
@@ -6,6 +6,10 @@
 
 Console.WriteLine("Enter number");
 int num = Convert.ToInt32(Console.ReadLine());
+char[] arr = num.ToString().ToCharArray();
+ReadArray(arr);
+//Console.WriteLine(arr[2]);
+FindNumber(arr);
 
 void ReadArray(char[]array)
 {
@@ -15,7 +19,11 @@ void ReadArray(char[]array)
     }
 }
 
-
-char[] arr = num.ToString().ToCharArray();
-ReadArray(arr);
-Console.WriteLine(arr[2]);
+void FindNumber(char[]array)
+{
+    for (int i=0 ; i<array.Length; i++)
+    if (array[i] >= 2)
+    Console.WriteLine(array[2]);
+    else 
+    Console.WriteLine("There is no third number");
+}
