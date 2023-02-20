ed4c521b6fc8bc84b2059d48d7fe2689be1e1365
Ekaterina Artamonova <ekaterina.artamonova16@gmail.com>
2023-02-20 16:57:32 +0500

    finished 15

--- Homework 2 seminar task 15/Program.cs
+++ Homework 2 seminar task 15/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Input number");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 6:
+    Console.WriteLine("Saturday");
+    break;
+    
+    case 7:
+    Console.WriteLine("Sunday");
+    break;
+    
+    default:
+    Console.WriteLine("This is not weekend");
+    break;
+}
+
